df629c447a999a76d6637cb76912728654444916
HtwoOtwo <765724965@qq.com>
2025-03-10 16:51:48 +0800

    init

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ func_config="$1"
 func_root=$(dirname "$func_config")
 
 docker build -t yolo .
-nuctl create yolo # create project yolo
+nuctl create project yolo # create project yolo
 nuctl deploy --project-name yolo --path "$func_root" --file "$func_config" --platform local
